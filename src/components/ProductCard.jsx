ca62cee988f650886bf708cbdb8b73f1a2267ff0
FranciscoHeronildo <heronildo.2010@hotmail.com>
2024-04-22 10:56:14 -0300

    Finalizando o CRUD de produtos

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,8 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography, Button, Box } from "@mui/material";
 
-const ProductCard = ({ name, description, price, stock }) => {
+const ProductCard = ({ name, description, price, stock, onEdit, onDelete }) => {
   return (
-    <Card>
+    <Card sx={{ marginBottom: 1 }}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {name}
@@ -16,6 +16,26 @@ const ProductCard = ({ name, description, price, stock }) => {
         <Typography variant="body2" color="text.secondary">
           Estoque: {stock} unidades
         </Typography>
+        <Box display="flex" flexDirection="column" alignItems="flex-end">
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={onEdit}
+            sx={{ marginBottom: "1px" }}
+          >
+            Editar
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            fullWidth
+            onClick={onDelete}
+            sx={{ marginTop: "1px" }}
+          >
+            Excluir
+          </Button>
+        </Box>
       </CardContent>
     </Card>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box, Grid, Button } from "@mui/material";
+import { Box, Grid, Button, Modal, TextField, Typography } from "@mui/material";
 import Navbar from "../components/Navbar";
 import ProductCard from "../components/ProductCard";
 import api from "../services/api";
@@ -7,12 +7,25 @@ import { toast } from "react-toastify";
 
 const Dashboard = () => {
   const [products, setProducts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newProductData, setNewProductData] = useState({
+    name: "",
+    description: "",
+    price: 0,
+    stock: 0,
+  });
+  const [editingProductId, setEditingProductId] = useState(null);
+  const [editedProductData, setEditedProductData] = useState({
+    name: "",
+    description: "",
+    price: 0,
+    stock: 0,
+  });
 
   const allProducts = async () => {
     try {
       const response = await api.get("products/get-all-products");
       setProducts(response.data.data);
-      toast.success(response.data.message + "!");
     } catch (error) {
       console.error(error);
       toast.error("Falha ao encontrar produtos");
@@ -23,10 +36,39 @@ const Dashboard = () => {
     allProducts();
   }, []);
 
-  const handleAddProduct = () => {};
+  const handleAddProduct = () => {
+    setIsModalOpen(true);
+  };
 
-  const handleEdit = (productId) => {
-    console.log("Editar produto com ID:", productId);
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewProductData({
+      ...newProductData,
+      [name]: value,
+    });
+  };
+
+  const handleAddProductSubmit = async () => {
+    try {
+      const storageToken = localStorage.getItem("token");
+      const auth = {
+        headers: {
+          Authorization: `Bearer ${storageToken}`,
+        },
+      };
+
+      await api.post("products/create-product", newProductData, auth);
+      setIsModalOpen(false);
+      toast.success("Produto adicionado com sucesso!");
+      allProducts();
+    } catch (error) {
+      console.error(error);
+      toast.error("Falha ao adicionar o produto");
+    }
   };
 
   const handleDelete = async (productId) => {
@@ -37,6 +79,45 @@ const Dashboard = () => {
     } catch (error) {
       console.error(error);
       toast.error("Falha ao excluir o produto");
+    }
+  };
+
+  const handleEdit = (productId) => {
+    const productToEdit = products.find((product) => product.id === productId);
+    if (productToEdit) {
+      setEditedProductData({
+        name: productToEdit.name,
+        description: productToEdit.description,
+        price: productToEdit.price,
+        stock: productToEdit.stock,
+      });
+      setEditingProductId(productId);
+      setIsModalOpen(true);
+    } else {
+      console.error("Produto não encontrado para edição");
+    }
+  };
+
+  const handleEditProductSubmit = async () => {
+    try {
+      const storageToken = localStorage.getItem("token");
+      const auth = {
+        headers: {
+          Authorization: `Bearer ${storageToken}`,
+        },
+      };
+
+      await api.put(
+        `products/update-product/${editingProductId}`,
+        editedProductData,
+        auth
+      );
+      setIsModalOpen(false);
+      toast.success("Produto atualizado com sucesso!");
+      allProducts();
+    } catch (error) {
+      console.error(error);
+      toast.error("Falha ao atualizar o produto");
     }
   };
 
@@ -60,25 +141,77 @@ const Dashboard = () => {
                 description={product.description}
                 price={product.price}
                 stock={product.stock}
+                onEdit={() => handleEdit(product.id)}
+                onDelete={() => handleDelete(product.id)}
               />
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => handleEdit(product.id)}
-              >
-                Editar
-              </Button>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => handleDelete(product.id)}
-              >
-                Excluir
-              </Button>
             </Grid>
           ))}
         </Box>
       </Box>
+      <Modal open={isModalOpen} onClose={handleModalClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography variant="h5" fontWeight="bold">
+            {editingProductId ? "Editar Produto" : "Adicionar Produto"}
+          </Typography>
+          <TextField
+            name="name"
+            label="Nome do Produto"
+            value={newProductData.name}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            name="description"
+            label="Descrição"
+            value={newProductData.description}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            name="price"
+            label="Preço"
+            type="number"
+            value={newProductData.price}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            name="stock"
+            label="Estoque"
+            type="number"
+            value={newProductData.stock}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={
+              editingProductId
+                ? handleEditProductSubmit
+                : handleAddProductSubmit
+            }
+          >
+            {editingProductId ? "Salvar" : "Adicionar"}
+          </Button>
+        </Box>
+      </Modal>
     </div>
   );
 };
